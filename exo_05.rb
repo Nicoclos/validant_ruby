f539986885a6795a58b6b392e98f76913903aa73
nicoclos <n.bia@outlook.fr>
2021-10-05 11:14:28 +0200

    exo 5.2

--- exo_05.rb
+++ exo_05.rb
@@ -2,8 +2,6 @@ repetition_ask_string = "Combien de répétitions ?"
 puts repetition_ask_string
 repetition_ask_string = gets.chomp.to_i 
 
-a = repetition_ask_string
-
-a.times do
+repetition_ask_string.times do
     puts "Salut, ça farte ?"
 end
