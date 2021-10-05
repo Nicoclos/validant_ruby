50ecbcffbbd3017f8e44ac374cd42149618bb1ec
nicoclos <n.bia@outlook.fr>
2021-10-05 12:11:54 +0200

    exo 3.2

--- exo_03.rb
+++ exo_03.rb
@@ -1,4 +1,4 @@
 age_ask_string = "Salut ! Quelle est ton année de naissance ?"
 puts age_ask_string
 age_ask_string = gets.chomp.to_i 
-puts "Alors, en 2017 tu avais #{2017 - age_ask_string} !"
+puts "Alors, en 2017 tu avais #{2017 - age_ask_string} ans !"
